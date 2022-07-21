7b7d97ad62e54bf48d7249c05e6e3a31f296877b
Hamma Azarok <hammaagazarok@gmail.com>
2022-07-21 13:53:48 +0200

    add refresh functionalty

---
+++ src/refresh-scores.js
@@ -0,0 +1,23 @@
+import {addScore} from "./add-score.js";
+const loadScores = () => {
+    const ScoresContainer = document.getElementById('leaders-container');
+    ScoresContainer.innerHTML = '';
+  const getScoresData = async () => {
+    const request = await fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/KJxUTySzzZA6BH0fd9pM/scores/");
+    const data = await request.json();
+    return data.result;
+  };
+
+  getScoresData().then(
+    (value) => {
+      value.forEach((score, id) => {
+        addScore(score.user, score.score, id);
+      });
+    },
+    (error) => {
+      throw error;
+    }
+  );
+};
+
+export default loadScores;
